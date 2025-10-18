7ad57fa23304d0e07e24127d7f33d60e62989b09
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-18 13:17:28 +0300

    Created new Specialtyes model

---
+++ server/src/models/Specialties.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db'); 
+
+const Specialties = sequelize.define('Specialties', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'specialties',
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true,
+}); 
+
+module.exports = Specialties;
