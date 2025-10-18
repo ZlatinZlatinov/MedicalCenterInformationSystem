8597534a35f751bb14fbe1f80d3fedafc40fa1b2
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-18 15:47:12 +0300

    Created new utility function for parsing errors

---
+++ server/src/utils/errorParser.js
@@ -0,0 +1,13 @@
+function errorParser(eror) {
+    if (Array.isArray(eror)) {
+        return eror.map((e) => e.msg).join('\n');
+    } else if (eror.name == 'validationError') {
+        return Object.values(eror.errors).map(v => v.message).join('\n');
+    } else {
+        return eror.message;
+    }
+} 
+
+module.exports = {
+    errorParser
+}
