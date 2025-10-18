8619f0a8d567ace6913ad2a86aa07aed6f3e6efb
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-18 13:16:49 +0300

    Created new Nurse model

---
+++ server/src/models/Nurse.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');  
+
+const Nurse = sequelize.define('Nurse', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id',
+        },
+    },
+    departmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'departments',
+            key: 'id',
+        },
+    },
+    specialtyId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'specialties',
+            key: 'id',
+        },
+    },
+    licenseNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    experience: {
+        type: DataTypes.INTEGER,
+    },
+    profilePicture: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'default.jpg',
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+});
+
+module.exports = Nurse;
