c87fd0b3b329070329bf88590769792e1d3926d4
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-19 14:59:08 +0300

    Updated navbar, added logo

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,30 @@
 import { Link } from 'react-router';
+import { Activity } from 'lucide-react';
 
 function NavBar() {
     return (
-        <nav id='nav-bar'>
-            <div className="site-logo">
-             <Link to='/'>MediCare</Link>   
-            </div> 
+        <header>
+            <nav id='nav-bar'>
+                <div className="site-logo">
+                    <Link to='/'>
+                        <Activity />
+                        <span>MediCare</span>
+                    </Link>
+                </div>
 
-            <ul className="main-nav">
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
+                <ul className="main-nav">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                </ul>
 
-            <div className="auth-btns">
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
-            </div>
-        </nav>
+                <div className="auth-btns">
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register'>Register</Link>
+                </div>
+            </nav>
+        </header>
     );
-} 
+}
 
 export default NavBar;
