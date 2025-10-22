3dcb56b471cce4dce4925521d6fc118aa0cdb96f
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-22 21:54:50 +0300

    Updated expressInit to use routes

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -1,6 +1,8 @@
+const { authController } = require('../controllers/authController');
 
 
 const routes = require('express').Router();
 
+routes.use('/auth', authController);
 
-module.export = { routes };
+module.exports = { routes };
