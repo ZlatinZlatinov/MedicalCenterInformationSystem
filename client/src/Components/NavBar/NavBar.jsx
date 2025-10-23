3a8940cc172f9d310dcd24729d493818a11d110d
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-23 22:22:17 +0300

    Updated navbar html structure

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -3,26 +3,26 @@ import { Activity } from 'lucide-react';
 
 function NavBar() {
     return (
-        <header>
-            <nav id='nav-bar'>
-                <div className="site-logo">
-                    <Link to='/'>
-                        <Activity />
-                        <span>MediCare</span>
-                    </Link>
-                </div>
+        <header id='site-header'>
+            <div className="site-logo">
+                <Link to='/'>
+                    <Activity className='activity' />
+                    <span>MediCare</span>
+                </Link>
+            </div>
 
+            <nav id='nav-bar'>
                 <ul className="main-nav">
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
                 </ul>
-
-                <div className="auth-btns">
-                    <Link to='/login'>Login</Link>
-                    <Link to='/register'>Register</Link>
-                </div>
             </nav>
+
+            <div className="auth-btns">
+                <Link to='/login' className='btn orange-btn'>Login</Link>
+                <Link to='/register' className='btn blue-btn'>Register</Link>
+            </div>
         </header>
     );
 }
