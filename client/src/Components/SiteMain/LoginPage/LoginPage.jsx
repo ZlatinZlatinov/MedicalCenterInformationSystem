7100003e2cd78e3210e7a4176911ec91b71b9cc4
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-24 20:50:26 +0300

    Updated login and register page structure

--- client/src/Components/SiteMain/LoginPage/LoginPage.jsx
+++ client/src/Components/SiteMain/LoginPage/LoginPage.jsx
@@ -4,13 +4,13 @@ import { Activity, MoveLeft } from 'lucide-react';
 function LoginPage() {
     return (
         <section id="login-page">
-            <form id="login-form">
+            <form id="login-form" className="auth-form">
                 <div className="form-header">
                     <p className="form-logo">
-                        <Activity />
+                        <Activity className="activity"/>
                     </p>
-                    <h2>Welcome Back</h2>
-                    <p>Sign in to access your MediCare account</p>
+                    <h2 className="form-heading">Welcome Back</h2>
+                    <span>Sign in to access your MediCare account</span>
                 </div>
 
                 <div className="form-fields">
@@ -25,14 +25,14 @@ function LoginPage() {
                     </div>
 
                     <div className="form-btn">
-                        <input type="submit" value="Sign In" />
+                        <input type="submit" value="Sign In" className="auth-btn" />
                         <input type="button" value="Google" />
                     </div>
                 </div>
 
                 <div className="form-footer">
-                    <p>Don't have an account? <Link to='/register'>Register here</Link></p>
-                    <p><Link to='/'><MoveLeft/><span>Back To Home</span></Link></p>
+                    <p>Don't have an account? <Link to='/register' className="blue-redirect">Register here</Link></p>
+                    <p><Link to='/' className="form-redirect"><MoveLeft/><span>Back To Home</span></Link></p>
                 </div>
             </form>
         </section>
--- client/src/Components/SiteMain/RegisterPage/RegisterPage.jsx
+++ client/src/Components/SiteMain/RegisterPage/RegisterPage.jsx
@@ -4,13 +4,13 @@ import { Activity, MoveLeft } from 'lucide-react';
 function RegisterPage() {
     return (
         <section id="register-page">
-            <form id="register-form">
+            <form id="register-form" className='auth-form'>
                 <div className="form-header">
                     <p className="form-logo">
-                        <Activity />
+                        <Activity className="activity"/>
                     </p>
-                    <h2>Create Account</h2>
-                    <p>Join MediCare today</p>
+                    <h2 className='form-heading'>Create Account</h2>
+                    <span>Join MediCare today</span>
                 </div>
 
                 <div className="form-fields">
@@ -35,13 +35,13 @@ function RegisterPage() {
                     </div>
 
                     <div className="form-btn">
-                        <input type="submit" value="Create Account" />
+                        <input type="submit" className='auth-btn' value="Create Account" />
                     </div>
                 </div>
 
                 <div className="form-footer">
-                    <p>Already Have an account? <Link to='/login'>Sign In</Link></p>
-                    <p><Link to='/'><MoveLeft/><span>Back To Home</span></Link></p>
+                    <p>Already Have an account? <Link to='/login' className="blue-redirect">Sign In</Link></p>
+                    <p><Link to='/' className='form-redirect'><MoveLeft/><span>Back To Home</span></Link></p>
                 </div>
             </form>
         </section>
