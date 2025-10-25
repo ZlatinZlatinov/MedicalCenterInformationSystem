961722311b18d80fc128ad9aa4978d8baa74a0d6
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-25 22:16:15 +0300

    Created new auth service

---
+++ client/src/services/authService.js
@@ -0,0 +1,49 @@
+const END_POINT = "/api/auth";
+
+export async function loginUser(userData) {
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + END_POINT + '/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }); 
+
+    if(response.status !== 200) {
+        throw new Error("Login failed!");
+    } 
+
+    const data = await response.json(); 
+
+    return data;
+}
+
+export async function registerUser(userData) {
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + END_POINT + '/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 201) {
+        throw new Error(data.message);
+    }
+}
+
+export async function verifyEmail(verificationToken) {
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + END_POINT + '/verify-email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ verificationToken })
+    });
+
+    if (response.status !== 202) {
+        throw new Error("Email verification failed!");
+    }
+}
