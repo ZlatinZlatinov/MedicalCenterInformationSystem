20c0e18454724a934cf680f9e6a9062941b3269f
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-26 12:43:14 +0200

    Created new context for the user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import NavBar from "./Components/NavBar/NavBar";
 import SiteMain from "./Components/SiteMain/SiteMain";
 import Footer from "./Components/Footer/Footer";
+import { useState } from "react";
+import { UserContext } from "./Contexts/UserContext";
 
 function App() {
+  const [authUserData, setAuthUserData] = useState({
+    username: "Guest",
+    email: "",
+    accessToken: "",
+    isLoggedIn: false,
+  });
 
   return (
     <>
-      <NavBar />
-      <SiteMain />
-      <Footer />
+      <UserContext.Provider value={{ authUserData, setAuthUserData }}>
+        <NavBar />
+        <SiteMain />
+        <Footer />
+      </UserContext.Provider>
     </>
   )
 }
