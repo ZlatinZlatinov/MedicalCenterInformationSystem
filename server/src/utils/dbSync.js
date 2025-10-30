f8452d9fd9aed0b7e62202c4ba3e941d82541260
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-10-30 23:52:06 +0200

    Updated dbSync script

--- server/src/utils/dbSync.js
+++ server/src/utils/dbSync.js
@@ -1,6 +1,10 @@
 const { sequelize } = require('../config/db');
 const User = require('../models/User');
 const EmailToken = require('../models/EmailToken');
+const Specialties = require('../models/Specialties');
+const Departments = require('../models/Departments');
+const Doctor = require('../models/Doctor');
+const DoctorSchedule = require('../models/DoctorSchedule');
 
 async function syncDatabase(force = flase) {
     try {
