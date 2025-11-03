2885e34f521646954f0763c3c3a71283964e3f26
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-03 17:12:42 +0200

    Added new fields to Appointments model

--- server/src/models/Appointment.js
+++ server/src/models/Appointment.js
@@ -18,10 +18,24 @@ const Appointments = sequelize.define('Appointments', {
         },
     },
     patientId: {
+        type: DataTypes.UUID,
         references: {
             model: 'users',
             key: 'id',
         },
+    },
+    appointmentDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    appointmentTime: {
+        type: DataTypes.TIME,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'confirmed', 'completed', 'canceled', 'no-show'),
+        allowNull: false,
+        defaultValue: 'pending'
     },
     isInitial: {
         type: DataTypes.BOOLEAN
@@ -29,9 +43,28 @@ const Appointments = sequelize.define('Appointments', {
     isNzok: {
         type: DataTypes.BOOLEAN
     },
+    duraion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 30
+    },
     price: {
         type: DataTypes.DECIMAL,
+        allowNull: false,
         defaultValue: 0
+    },
+    cancelledAt: {
+        type: DataTypes.DATE
+    },
+    cancelledBy: {
+        type: DataTypes.UUID,
+        references: {
+            model: 'users',
+            key: 'id',
+        },
+    },
+    cancellationReason: {
+        type: DataTypes.TEXT
     },
     dateTime: {
         type: DataTypes.DATE
