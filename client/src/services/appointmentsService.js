2a23d27e05613c08fc248c9a52a13fccfd33f95a
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-09 22:42:41 +0200

    Created appointments service, now users can book appointments

---
+++ client/src/services/appointmentsService.js
@@ -0,0 +1,21 @@
+const END_POINT = "/api/appointments";
+
+export async function bookAppointment(payload) {
+    console.log(payload);
+    
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + END_POINT + '/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+
+    if (response.status !== 201) {
+        throw new Error("Failed to book an appointment");
+    }
+
+    const data = await response.json();
+
+    return data;
+}
