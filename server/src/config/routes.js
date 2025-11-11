56f6ee1a79306d73d520a905d96c795d779100ce
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-11 22:47:21 +0200

    Added new middleware for doctor route

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -1,12 +1,13 @@
 const { appontintmentsController } = require('../controllers/appointmentsController');
 const { authController } = require('../controllers/authController');
 const { doctorController } = require('../controllers/doctorController');
+const { isDoctor } = require('../middlewares/guard');
 
 
 const routes = require('express').Router();
 
 routes.use('/auth', authController);
-routes.use('/doctor', doctorController);
+routes.use('/doctor', isDoctor(), doctorController);
 routes.use('/appointments', appontintmentsController);
 
 module.exports = { routes };
