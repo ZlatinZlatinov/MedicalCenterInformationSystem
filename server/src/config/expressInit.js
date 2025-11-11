878e884b5252a7c36fc69cb2fdfc321ee26d6966
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-11 22:58:30 +0200

    Added new package helmet

--- server/src/config/expressInit.js
+++ server/src/config/expressInit.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const { routes } = require('./routes');
 
 function expressInit(app) {
+    app.use(helmet());
     app.use(express.json());
     app.use(cors({
         origin: [process.env.CLIENT_URL],
