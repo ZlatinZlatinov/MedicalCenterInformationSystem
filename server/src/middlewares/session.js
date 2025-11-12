2aac27dda64e2b278a4c0920cb105769dadd7d15
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-12 21:57:18 +0200

    Fix session middleware to use bearer token

--- server/src/middlewares/session.js
+++ server/src/middlewares/session.js
@@ -1,17 +1,19 @@
 const { parseToken } = require("../utils/jwt");
 
 module.exports = () => (req, res, next) => {
-    const token = req.headers['authorization']; 
-
-    if(token) {
+    const authHeader = req.headers['authorization'];
+    
+    if (authHeader) {
         try {
+            const token = authHeader.split(' ')[1];
             const payload = parseToken(token);
+
             req.user = payload;
             req.token = token;
         } catch (err) {
-            return res.status(401).json({message: "Invalid token!"});
+            return res.status(401).json({ message: "Invalid token!" });
         }
-    } 
+    }
 
     next();
 }
