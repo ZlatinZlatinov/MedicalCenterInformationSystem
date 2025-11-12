a98acce87da8767f18eca50c9f40b7377b6f29f7
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-12 21:52:11 +0200

    Created AuthProvider component for auth context, created useAuth hook

--- client/src/Contexts/UserContext.jsx
+++ client/src/Contexts/UserContext.jsx
@@ -1,10 +1,47 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect, useMemo } from "react";
 
-export const UserContext = createContext({
-    id: "",
-    role: "",
-    username: "Guest",
-    email: "",
-    accessToken: "",
-    isLoggedIn: false,
-});
+export const UserContext = createContext();
+//TODO: create user persisted state, when user refreshes the page
+function AuthProvider({ children }) {
+    const [authUserData, setAuthUserData_] = useState({
+        id: '',
+        role: '',
+        email: '',
+        username: '',
+        isLoggedIn: false,
+        accessToken: sessionStorage.getItem("accessToken")
+    });
+
+    const setAuthUserData = (newState) => {
+        if (newState) {
+            setAuthUserData_(newState);
+        } else {
+            setAuthUserData_({
+                id: '',
+                role: '',
+                email: '',
+                isLoggedIn: false,
+                accessToken: sessionStorage.getItem("accessToken")
+            })
+        }
+    }
+
+    useEffect(() => {
+        if (authUserData.accessToken) {
+            sessionStorage.setItem("accessToken", authUserData.accessToken);
+        } else {
+            sessionStorage.removeItem("accessToken");
+        }
+    }, [authUserData]);
+
+    const contextValue = useMemo(() => ({
+        authUserData,
+        setAuthUserData
+    }), [authUserData])
+
+    return (
+        <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+    );
+}
+
+export default AuthProvider;
