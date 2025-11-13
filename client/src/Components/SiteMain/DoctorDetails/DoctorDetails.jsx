7574ea89acab42e77d3e6e7739b62fbec2521f62
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-13 22:25:38 +0200

    Updated DoctorDetails component to use doctor service

--- client/src/Components/SiteMain/DoctorDetails/DoctorDetails.jsx
+++ client/src/Components/SiteMain/DoctorDetails/DoctorDetails.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { CircleCheck, Calendar, CircleX } from "lucide-react";
-import { useState } from "react";
 import CalendarApp from "./Calendar";
+import { getDoctorById } from "../../../services/doctorService";
 
 function DoctorDetails() {
     const params = useParams();
     const [isHidden, setIsHidden] = useState(true);
-    console.log(params.doctorId);
+    const [doctorDetails, setDoctrorDetails] = useState({
+        imgSrc: '', doctorName: '', department: '', specialty: '', experience: 0, description: '', education: ''
+    });
+    const doctorId = params.doctorId;
+
+    useEffect(() => {
+        async function fetchDoctorDetails() {
+            try {
+                const data = await getDoctorById(doctorId);
+                setDoctrorDetails(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchDoctorDetails()
+    }, [doctorId]);
 
     return (
         <section id="doctor-details">
@@ -20,18 +37,16 @@ function DoctorDetails() {
                         </div>
 
                         <div className="main-info-heading">
-                            <h3 className="main-info-title">Dr. Jeff Bezos</h3>
-                            <span className="main-infor-department">Cariology</span> {/*Department */}
-                            <p className="doctor-card-experience">15 years experience</p>
+                            <h3 className="main-info-title">Dr. {doctorDetails.doctorName}</h3>
+                            <span className="main-infor-department">{doctorDetails.department}</span> {/*Department */}
+                            <span className="main-infor-specialty">{doctorDetails.specialty}</span> {/*Specialty */}
+                            <p className="doctor-card-experience">{doctorDetails.experience} years experience</p>
                         </div>
                     </div>
 
                     <div className="main-info-details">
                         <h4>About</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Laboriosam unde tenetur, voluptatibus, perferendis, deleniti
-                            doloribus quis illo tempore ipsum in amet incidunt provident
-                            nobis nulla dolorem quae id eos iure.</p>
+                        <p>{doctorDetails.description}</p>
                     </div>
                 </div>
 
@@ -52,7 +67,7 @@ function DoctorDetails() {
                 <div className="book-doctor">
                     <div className="book-doctor-heading">
                         <h4><Calendar color="#3c83f6" size={24} />Book Appointment</h4>
-                        <span>Schedule with Dr. Jeff Bezos</span>
+                        <span>Schedule with Dr. {doctorDetails.doctorName}</span>
                     </div>
 
                     <div className="book-doctor-footer">
