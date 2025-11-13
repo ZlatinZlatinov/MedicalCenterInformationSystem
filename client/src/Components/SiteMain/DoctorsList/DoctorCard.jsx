e6db62de3a3f3e9773cf96975ec9760ac55d4bef
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-13 21:32:43 +0200

    Created DoctorCard Component

---
+++ client/src/Components/SiteMain/DoctorsList/DoctorCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+
+function DoctorCard({ doctorId, imgSrc, doctorName, department, specialty, experience }) {
+    return (
+        <div className="doctor-card">
+            <div className="doctor-card-header">
+                <div className="doctor-card-img">
+                    <img src={"./public/images/bezos.png"} alt="Doctor profile picture" />
+                </div>
+                <div className="doctor-card-heading">
+                    <h4 className="doctor-card-title">Dr. {doctorName}</h4>
+                    <span className="doctor-card-department">{department}</span> {/*Department */}
+                    <span className="doctor-card-specialty">{specialty}</span> {/*Specialty */}
+                </div>
+            </div>
+
+            <div className="doctor-card-content">
+                <p className="doctor-card-experience">{experience} years experience</p>
+                <Link to={`/${doctorId}`} className="doctor-card-details">View Details and Book</Link>
+            </div>
+        </div>
+    );
+}
+
+export default DoctorCard;
