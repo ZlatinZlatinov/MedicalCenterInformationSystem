f05adbbf8851ae25e41e1642de88abe7f9c740b3
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-13 19:09:46 +0200

    Updated doctor controller, added get doctors by id and by query parameters

--- server/src/controllers/doctorController.js
+++ server/src/controllers/doctorController.js
@@ -1,16 +1,18 @@
+const doctorController = require('express').Router();
+const { body, validationResult, param, header, query } = require('express-validator');
+
+const { isDoctor } = require('../middlewares/guard');
 const { createScheduleForAllDays } = require('../services/doctorSchedule');
+const { getDoctorById, getDoctorsByFilters } = require('../services/doctorService');
 const { errorParser } = require('../utils/errorParser');
 
-const doctorController = require('express').Router();
-const { body, validationResult, param, header } = require('express-validator');
-
-doctorController.post('/schedule',
-    header('Authorization').trim().notEmpty().isJWT()
+doctorController.post('/schedule', isDoctor(),
+    header('Authorization').trim().notEmpty().isJWT().bail()
         .withMessage("Authorization header is required!"),
     body('weekdays').notEmpty().isArray()
-        .withMessage("Enter valid weekdays format"),
+        .withMessage("Enter valid weekdays format!"),
     body('duration').isInt({ gt: 0, lt: 60 })
-        .withMessage("Enter valid schedule duration"),
+        .withMessage("Enter valid schedule duration!"),
     body('isFree').isBoolean()
         .withMessage("Enter valid boolean option!"),
     body('price').isFloat({ gt: 0, lt: 500 })
@@ -31,7 +33,60 @@ doctorController.post('/schedule',
         } catch (error) {
             const message = errorParser(error);
             console.log("Oops, something went wrong: ", message);
-            res.status(500).json({ message });
+            res.status(400).json({ message });
+        }
+    });
+
+// Get doctor details
+doctorController.get('/:doctorId',
+    param('doctorId').trim().isInt()
+        .withMessage("Invalid doctor ID"),
+    async (req, res) => {
+        const doctorId = req.params.doctorId;
+
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array());
+            }
+
+            const payload = await getDoctorById(doctorId);
+            res.json(payload);
+        } catch (error) {
+            console.log("Oops, something went wrong, ", error);
+            const message = errorParser(error);
+            res.status(404).json({ message });
+        }
+    });
+
+//Get all doctors 
+doctorController.get('/',
+    query('departmentId').optional().isInt()
+        .withMessage('Invalid department id'),
+    query('specialtyId').optional().isInt()
+        .withMessage('Invalid specialty id'),
+    async (req, res) => {
+        const query = req.query;
+    
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array());
+            }
+
+            const filters = {
+                departmentId: query.departmentId,
+                specialtyId: query.specialtyId,
+            }; 
+
+            const payload = await getDoctorsByFilters(filters);
+            res.json(payload);
+        } catch (error) {
+            console.log("Oops, something went wrong, ", error);
+            const message = errorParser(error);
+            res.status(404).json({ message });
         }
     });
 
