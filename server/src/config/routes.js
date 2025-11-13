df6624373d6bb610e5f9af5ee192db4c25437458
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-13 19:08:42 +0200

    Fix routes, removed isDoctor

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -1,13 +1,11 @@
 const { appontintmentsController } = require('../controllers/appointmentsController');
 const { authController } = require('../controllers/authController');
 const { doctorController } = require('../controllers/doctorController');
-const { isDoctor } = require('../middlewares/guard');
-
 
 const routes = require('express').Router();
 
 routes.use('/auth', authController);
-routes.use('/doctor', isDoctor(), doctorController);
+routes.use('/doctor', doctorController);
 routes.use('/appointments', appontintmentsController);
 
 module.exports = { routes };
