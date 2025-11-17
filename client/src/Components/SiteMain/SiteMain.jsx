cc0a266a4547b29d1c63bc8e125f2b80ab955474
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-17 12:13:37 +0200

    Created authenticated routes only for logged in users and doctors

--- client/src/Components/SiteMain/SiteMain.jsx
+++ client/src/Components/SiteMain/SiteMain.jsx
@@ -11,6 +11,8 @@ import DoctorSchedule from './DoctorSchedule/DoctorScheDule';
 import DoctorDetails from './DoctorDetails/DoctorDetails';
 import DoctorsList from './DoctorsList/DoctorsList';
 import DoctorRegister from './DoctorRegister/DoctorRegister';
+import AuthenticatedUser from './ProtectedRoutes/AuthenticatedUser';
+import AuthenticatedDoctor from './ProtectedRoutes/AuthenticatedDoctor';
 
 function SiteMain() {
     return (
@@ -22,9 +24,18 @@ function SiteMain() {
                 <Route path='/login' element={<LoginPage />} />
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/verify-email' element={<VerifyEmail />} />
-                <Route path='/dashboard' element={<Dashboard />} /> {/*Should be protected*/}
-                <Route path='/schedule' element={<DoctorSchedule />} /> {/*Should be protected*/}
-                <Route path='/become-a-doctor' element={<DoctorRegister />} /> {/*Should be protected*/}
+
+                {/* Protected Routes for Users */}
+                <Route element={<AuthenticatedUser />}>
+                    <Route path='/dashboard' element={<Dashboard />} /> {/*Should be protected*/}
+                    <Route path='/become-a-doctor' element={<DoctorRegister />} /> {/*Should be protected*/}
+                </Route>
+
+                {/* Protected Routes for Doctors */}
+                <Route element={<AuthenticatedDoctor />}>
+                    <Route path='/schedule' element={<DoctorSchedule />} /> {/*Should be protected*/}
+                </Route>
+
                 <Route path='/doctors' element={<DoctorsList />} />
                 <Route path='/doctors/:doctorId' element={<DoctorDetails />} />
             </Routes>
