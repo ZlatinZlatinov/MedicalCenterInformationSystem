1158093a6aac93dfcd6e7fb8259c08809e7553cb
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-24 12:24:42 +0200

    Fix DoctorsList component, added all specialties and departments to the select options and added search params

--- client/src/Components/SiteMain/DoctorsList/DoctorsList.jsx
+++ client/src/Components/SiteMain/DoctorsList/DoctorsList.jsx
@@ -1,14 +1,26 @@
 import { useEffect, useState } from "react";
 import { getAllDoctors } from "../../../services/doctorService";
 import DoctorCard from "./DoctorCard";
+import { departmentNames, specialtyNames } from "../../../Constants/departments";
+import { useSearchParams } from "react-router";
 
 function DoctorsList() {
+    const [searchParams, setSearchParams] = useSearchParams();
     const [doctorsList, setDoctorsList] = useState([]);
+
+    const department = searchParams.get('department') || "";
+    const specialty = searchParams.get('specialty') || "";
+
+    function handleOnchange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        setSearchParams({ [name]: value });
+    }
 
     useEffect(() => {
         async function fetchDoctors() {
             try {
-                const data = await getAllDoctors();
+                const data = await getAllDoctors(department, specialty);
                 setDoctorsList(data);
             } catch (error) {
                 console.error(error);
@@ -17,7 +29,7 @@ function DoctorsList() {
         }
 
         fetchDoctors();
-    }, []);
+    }, [department, specialty]);
 
     return (
         <section id="doctors-list">
@@ -25,14 +37,19 @@ function DoctorsList() {
                 <h2>Browse Doctors</h2>
                 <div className="search-doctors">
                     <input type="search" name="searchDoctors" id="searchDoctors" placeholder="Search doctors..." />
-                    <select name="specialties" id="specialties">
-                        <option value="All Specialties">All Specialties</option>
-                        <option value="Cardiology">Cardiology</option>
+
+                    <select name="department" id="department" onChange={handleOnchange} value={department}>
+                        <option value="All Departments">All Departments</option>
+                        {departmentNames.map((d, index) => {
+                            return <option value={d} key={index}>{d}</option>
+                        })}
                     </select>
 
-                    <select name="departments" id="departments">
-                        <option value="All Departments">All Departments</option>
-                        <option value="Cardiology">Cardiology</option>
+                    <select name="specialty" id="specialty" onChange={handleOnchange} value={specialty}>
+                        <option value="All Specialties">All Specialties</option>
+                        {specialtyNames.map((s, index) => {
+                            return <option value={s} key={index}>{s}</option>
+                        })}
                     </select>
                 </div>
             </div>
