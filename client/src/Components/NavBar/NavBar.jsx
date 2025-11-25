b96991c9396686decfb7ea2ce80f3d08be765979
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-25 21:57:30 +0200

    Updated navbar

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -32,8 +32,8 @@ function NavBar() {
                 <ul className="main-nav">
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li> 
                     <li><Link to='/doctors'>Find a Doctor</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li> 
                 </ul>
             </nav>
 
