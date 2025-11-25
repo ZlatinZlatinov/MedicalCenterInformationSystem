9f8be9b8edb78f5d8cbdd508b85f740d9d6af361
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-11-25 21:49:47 +0200

    Updated admin controller, added /staff endpoint to get staff by filters

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -1,4 +1,33 @@
-const adminController = require('express').Router(); 
+const { getPendingUsers } = require('../services/adminService');
+const { errorParser } = require('../utils/errorParser');
+
+const adminController = require('express').Router();
+
+adminController.get('/staff', async (req, res) => {
+    const { filter } = req.query;
+    
+    //TODO: Make sure user id coresponds to the actual user
+    let payload = [];
+
+    try {
+        switch (filter) {
+            case 'pending':
+            case 'doctors':
+            case 'declined':
+            case 'approved':
+                payload = await getPendingUsers(filter);
+                break;
+            case 'nurses': break;
+            default: break;
+        }
+
+        res.json(payload);
+    } catch (error) {
+        console.error(error);
+        const message = errorParser(error);
+        res.json({ message });
+    }
+});
 
 module.exports = {
     adminController
