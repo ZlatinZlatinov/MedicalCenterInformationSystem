e9259703ff90208d9cf02792366b278eeb1399a3
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2026-09-21 18:21:03 +0300

    Fix import in Document model

--- server/src/models/Document.js
+++ server/src/models/Document.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
+const User = require('./User');
 
 const Document = sequelize.define('Document', {
     documentId: {
